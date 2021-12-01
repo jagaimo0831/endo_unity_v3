82c2d6ca08c9422d90d23e0df427381936a0b8c1
ken <ken@mizuuchi.lab.tuat.ac.jp>
2021-12-01 16:25:24 +0900

    add RelaySwitchingProgram

--- VirtualWings_v3/Assets/Scripts/Arduino/Transistor_Switching/Transistor_Switching.ino
+++ VirtualWings_v3/Assets/Scripts/Arduino/Transistor_Switching/Transistor_Switching.ino
@@ -1,18 +1,22 @@
-const int gate_pin = 11;
+const int GATE_PIN = 11;
+//const int LED_PIN = 13;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(gate_pin, OUTPUT);
+  pinMode(GATE_PIN, OUTPUT);
+  //pinMonde(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("HIGH");
-  digitalWrite(gate_pin, HIGH);
+  digitalWrite(GATE_PIN, HIGH);
+  //digitalWrite(LED_PIN, HIGH);
   delay(3000);
 
   Serial.println("LOW");
-  digitalWrite(gate_pin, LOW);
+  digitalWrite(GATE_PIN, LOW);
+  //digitalWrite(LED_PIN, LOW);
   delay(3000);
 }
